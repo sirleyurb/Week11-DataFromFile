c24fa9ecc9e2e44d5dd8a08cce15058f3d6c88e8
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:09:07 +0200

    week-11

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color:");
+            string userColor = Console.ReadLine();
+
+            string output = GetLineFromfileData(userColor);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Color not found.");
+            }
+
+
+        }
+
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\samples";
+            string fileName = "personality.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLineFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your zodiac sign:");
+            string userZodiacSign = Console.ReadLine();
+
+            string output = GetLineFromfileData(userZodiacSign);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Zodiac sign not found.");
+            }
+
+            static string[] ReadDataFromFile()
+            {
+                string directoryPath = @"C:\Users\opilane\samples";
+                string fileName = "horoscope.txt";
+                string fullPath = $@"{directoryPath}\{fileName}";
+
+                string[] dataFromFile = File.ReadAllLines(fullPath);
+
+                return dataFromFile;
+            }
+
+            static string GetLineFromfileData(string userInput)
+            {
+                string[] dataFromFile = ReadDataFromFile();
+                string result = "";
+
+                foreach (string line in dataFromFile)
+                {
+                    if (line.Contains(userInput))
+                    {
+                        result = line;
+                        break;
+                    }
+
+                }
+
+                return result;
+
+            }
+        }
+    }
+}
